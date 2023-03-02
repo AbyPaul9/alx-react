ea616d429f90fe89948c7dbd0f49330fb53f05bb
AbyPaul9 <akandeabayomipaul@gmail.com>
2023-03-02 04:35:07 -0800

    Task 5

---
+++ 0x02-react_props/task_5/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,44 @@
+
+Zelipha
+/
+alx-react
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+Beta Try the new code view
+alx-react/0x02-react_props/task_5/dashboard/src/CourseList/CourseListRow.test.js
+@Zelipha
+Zelipha Shapes, Loops, and Keys
+ 1 contributor
+26 lines (20 sloc)  1.04 KB
+import React from "react";
+import CourseListRow from "./CourseListRow";
+import { shallow } from "enzyme";
+
+describe("Course List Row component test", () => {
+  it("should render without crashing", () => {
+    const wrapper = shallow(<CourseListRow textFirstCell="test" />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("should render one cell with colspan = 2 when textSecondCell null", () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell={null} />);
+
+    expect(wrapper.find("tr").children()).toHaveLength(1);
+    expect(wrapper.find("tr").childAt(0).html()).toEqual('<th colSpan="2">test</th>');
+  });
+
+  it("should render two cells when textSecondCell not null", () => {
+    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell="test" />);
+
+    expect(wrapper.find("tr").children()).toHaveLength(2);
+    expect(wrapper.find("tr").childAt(0).html()).toEqual("<td>test</td>");
+    expect(wrapper.find("tr").childAt(1).html()).toEqual("<td>test</td>");
+  });
+});
